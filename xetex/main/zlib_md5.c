b24e6f7343a31a046a15f23a53c18c05e8c8d328
Frédéric Bour <frederic.bour@lakaban.net>
2025-02-09 10:38:31 +0900

    MD5: read from abstract input

--- xetex/main/zlib_md5.c
+++ xetex/main/zlib_md5.c
@@ -2,20 +2,29 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <zlib.h>
+#include "tectonic_bridge_core.h"
 #include "tectonic_bridge_flate.h"
 #include "tectonic_bridge_core_generated.h"
 
-FlateResult tectonic_flate_compress(uint8_t *output_ptr, uint64_t *output_len, const uint8_t *input_ptr, uint64_t input_len, uint32_t compression_level)
+FlateResult tectonic_flate_compress(uint8_t *output_ptr,
+                                    uint64_t *output_len,
+                                    const uint8_t *input_ptr,
+                                    uint64_t input_len,
+                                    uint32_t compression_level)
 {
   abort();
 }
 
-FlateResult tectonic_flate_decompress(uint8_t *output_ptr, unsigned long *output_len, const uint8_t *input_ptr, uint64_t input_len)
+FlateResult tectonic_flate_decompress(uint8_t *output_ptr,
+                                      unsigned long *output_len,
+                                      const uint8_t *input_ptr,
+                                      uint64_t input_len)
 {
   return uncompress(output_ptr, output_len, input_ptr, input_len);
 }
 
-void *tectonic_flate_new_decompressor(const uint8_t *input_ptr, uint64_t input_len)
+void *tectonic_flate_new_decompressor(const uint8_t *input_ptr,
+                                      uint64_t input_len)
 {
   z_stream *strm = calloc(sizeof(z_stream), 1);
   int ret = inflateInit(strm);
@@ -29,7 +38,9 @@ void *tectonic_flate_new_decompressor(const uint8_t *input_ptr, uint64_t input_l
   return strm;
 }
 
-int tectonic_flate_decompress_chunk(void *handle, uint8_t *output_ptr, uint64_t *output_len)
+int tectonic_flate_decompress_chunk(void *handle,
+                                    uint8_t *output_ptr,
+                                    uint64_t *output_len)
 {
   z_stream *strm = handle;
   strm->avail_out = *output_len;
@@ -48,7 +59,9 @@ void tectonic_flate_free_decompressor(void *handle)
 
 #include "picohash.h"
 
-int ttbc_get_data_md5(const uint8_t *data, size_t len, uint8_t *digest)
+int ttbc_get_data_md5(const uint8_t *data,
+                      size_t len,
+                      uint8_t *digest)
 {
   _picohash_md5_ctx_t ctx;
   _picohash_md5_init(&ctx);
@@ -57,10 +70,12 @@ int ttbc_get_data_md5(const uint8_t *data, size_t len, uint8_t *digest)
   return 1;
 }
 
-int ttstub_get_file_md5(char const *path, char *digest)
+int ttstub_get_file_md5(const char *path,
+                        char *digest)
 {
-  FILE *f = fopen(path, "rb");
-  if (!f)
+  ttbc_input_handle_t *input = ttstub_input_open(path, TTBC_FILE_FORMAT_PROGRAM_DATA, 0);
+
+  if (!input)
     return 0;
   _picohash_md5_ctx_t ctx;
   _picohash_md5_init(&ctx);
@@ -68,14 +83,10 @@ int ttstub_get_file_md5(char const *path, char *digest)
   char buf [4096];
   size_t r;
 
-  do
-  {
-    r = fread(buf, 1, 4096, f);
+  while ((r = ttstub_input_read(input, buf, 4096)) > 0)
     _picohash_md5_update(&ctx, buf, r);
-  }
-  while (r > 0);
 
-  fclose(f);
+  ttstub_input_close(input);
 
   _picohash_md5_final(&ctx, digest);
 
